c005bd4ff826dd3ea3985fd3a8d19edc1555c6e6
Antony <arantony@augustasoftsol.com>
2018-09-17 17:51:26 +0530

    log update

---
+++ FMCApp.Core/Dtos/Log/LogsDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace FMCApp.Core.Dtos.Log
+{
+    public class LogsDto
+    {
+        public LogsDto()
+        {
+            Logs = new List<LogDto>();
+        }
+
+        public List<LogDto> Logs { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
